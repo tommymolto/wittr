6564c693fe716f81e3ddf236970f8611827ba17e
paulo marinho <paulo.marinho@personates.com>
2017-04-07 00:12:20 -0300

    traducao

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -14,6 +14,6 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  // TODO: respond with an entry from the cache if there is one.
-  // If there isn't, fetch from the network.
+  // TODO: se tem no cache, responda com ela
+  // Senão, pegue da internet
 });
